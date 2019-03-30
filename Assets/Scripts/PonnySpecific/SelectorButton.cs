9a6a35ab8af65fbdb559af766633b1df7fb4f905
indianaj0nas <indianajonas42@gmail.com>
2019-03-30 12:32:21 +0100

    Forgot to select files in previous commit.

---
+++ Assets/Scripts/PonnySpecific/SelectorButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorButton : MonoBehaviour
+{
+    public int stateNumber;
+    public GameObject horsePartHandler;
+
+    void Start()
+    {
+        gameObject.AddComponent<BoxCollider2D>();
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    void OnMouseDown()
+    {
+        SendStateChange();
+    }
+
+    void SendStateChange()
+    {
+        horsePartHandler.GetComponent<HorsePart>().SetState(stateNumber);
+    }
+}
