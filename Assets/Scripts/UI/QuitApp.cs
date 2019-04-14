36aa3b46e24406b624bfa086834b3737b47f1bac
indianaj0nas <indianajonas42@gmail.com>
2019-04-14 10:05:45 +0200

    Finished the main menu and a few other UI tweaks.

---
+++ Assets/Scripts/UI/QuitApp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
