f9abb3a8493e5ad8214c553c2a2ba03c2f3fd23b
indianaj0nas <indianajonas42@gmail.com>
2019-04-25 19:21:27 +0200

    Reorganized some art in folders. Made a first draft of horses in the dictionary.

---
+++ Assets/Scripts/Uppslagsverk/OnClickEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnClickEvent : MonoBehaviour
+{
+
+    public UnityEvent onClick;
+
+    void OnMouseDown()
+    {
+        onClick.Invoke();
+    }
+}
