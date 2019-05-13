c21ee92d4ca4ceb3b398c9fec4708555fe0bb0b3
indianaj0nas <indianajonas42@gmail.com>
2019-05-13 19:58:36 +0200

    I made a nice scene-transition

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour {
+public class ChangeScene : MonoBehaviour
+{
+    private string sceneToLoad;
 
-	public void LoadScene(string sceneName)
-	{
-		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-	}
+    public void LoadScene(string sceneName)
+    {
+        GameObject sceneTransition = GameObject.Find("Canvas/SceneTransition");
+        sceneTransition.GetComponent<Animator>().SetTrigger("FadeOut");
+        sceneTransition.gameObject.SetActive(true);
+        sceneToLoad = sceneName;
+        Invoke("Load", 0.1f);
+    }
+
+    void Load()
+    {
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+
+
 }
---
+++ Assets/Scripts/UI/InitiateScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitiateScene : MonoBehaviour
+{
+    public GameObject sceneTransition;
+
+    void Start()
+    {
+        sceneTransition.SetActive(true);
+    }
+}
