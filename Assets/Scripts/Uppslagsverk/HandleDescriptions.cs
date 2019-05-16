4c73e69a6218b88efb759172aed1629c8ae47c6a
indianaj0nas <indianajonas42@gmail.com>
2019-05-16 19:49:34 +0200

    Remade the system for changing descriptions in Uppslagsverket and added nice UI tweaks.

---
+++ Assets/Scripts/Uppslagsverk/HandleDescriptions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleDescriptions : MonoBehaviour
+{
+
+    public GameObject[] deactivateThese;
+    public GameObject[] activateThese;
+    public GameObject hoverIndicator;
+
+    void OnMouseDown()
+    {
+        if (hoverIndicator != null)
+            hoverIndicator.gameObject.SetActive(false);
+			
+        for (int i = 0; i < activateThese.Length; i++)
+        {
+            activateThese[i].gameObject.SetActive(true);
+        }
+
+        for (int i = 0; i < deactivateThese.Length; i++)
+        {
+            deactivateThese[i].gameObject.SetActive(false);
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (hoverIndicator != null)
+            hoverIndicator.gameObject.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        if (hoverIndicator != null)
+            hoverIndicator.gameObject.SetActive(false);
+    }
+}
