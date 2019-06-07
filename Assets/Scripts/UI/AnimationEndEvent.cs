4d33045f6a3353e67f2cf5535476720ffaed1e9f
indianaj0nas <indianajonas42@gmail.com>
2019-06-07 14:16:44 +0200

    Had to make a new splashscreen menu

---
+++ Assets/Scripts/UI/AnimationEndEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEndEvent : MonoBehaviour
+{
+    public UnityEvent onAnimationEnd;
+
+    private Animator animator;
+    private bool animationDone;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("NoState") && !animationDone)
+        {
+            onAnimationEnd.Invoke();
+        }
+    }
+}
--- Assets/Scripts/UI/InitiateScene.cs
+++ Assets/Scripts/UI/InitiateScene.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class InitiateScene : MonoBehaviour
 {
     public GameObject sceneTransition;
+    public bool useFadeIn;
+
+    private Animator animator;
 
     void Start()
     {
+        animator = sceneTransition.GetComponent<Animator>();
+
         sceneTransition.SetActive(true);
+        
+        if (useFadeIn == true)
+            animator.SetBool("UseFadeIn", true);
+        else
+            animator.SetBool("UseFadeIn", false);
+
     }
 }
